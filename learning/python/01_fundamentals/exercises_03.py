4767ef8863d08e314ed4398c9dc0c83abf5e631e
PedroAugusto2101 <pedrotiagobh@gmail.com>
2025-01-12 22:51:22 -0300

    test

--- learning/python/01_fundamentals/exercises_03.py
+++ learning/python/01_fundamentals/exercises_03.py
@@ -4,10 +4,6 @@ segundo_valor = input('Digite outro valor: ')
 if primeiro_valor >= segundo_valor:
     print(f'{primeiro_valor=} é maior ou igual que o {segundo_valor=}')
 else:
-<<<<<<< HEAD
     print(f'{segundo_valor=} é maior que o {primeiro_valor=}')
 
-    ## testttttfdfdfaaaaa
-=======
-    print(f'{segundo_valor=} é maior que o {primeiro_valor=}')
->>>>>>> bb6edb4c250d7228823a2d809abc654ba6a58b14
+#test
